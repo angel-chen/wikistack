11c56c3b312d0ac6cc9dee4ad7d274a11c2d7145
angel-chen <rikugou@gmail.com>
2018-04-27 12:27:44 -0400

    Created models and connected database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./views/index');
 const layout = require('./views/layout');
+const { db, Page, User } = require('./models');
 
 const app = express();
 
@@ -16,6 +17,17 @@ app.use('/', (req, res) => {
   res.send(routes.main());
 })
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+const init = async(req, res) => {
+  await db.sync({force: true});
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  })
+}
+
+init();
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
 })
